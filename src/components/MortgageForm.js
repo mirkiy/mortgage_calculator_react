a86df18f3f4a5ae646cd5e2a0bc3e425575ae0c2
mirkiy <mirizachar@gmail.com>
2023-05-31 17:00:45 +0100

    work on the form component

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -1,6 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MortgageForm = () => {
-  return <div className="mortgageForm">Hello from form</div>;
+  const [formData, setFormData] = useState({
+    salary1: "",
+    salary2: "",
+    deposit: "",
+  });
+  const handleChange = (event) => {
+    const newData = {...formData}
+    newData[event.target.name] = event.target.value
+    setFormData(newData)
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="mortgageForm">
+      <form>
+        <div className="formWrap">
+        <label>Your salary:</label>
+          <input
+            onChange={handleChange}
+            min="O"
+            type="number"
+            id="salary1"
+            name="salary1"
+            value={formData.salary1}
+          />
+        </div>
+        <div className="formWrap">
+        <label>Your partner's salary:</label>
+        <input
+            onChange={handleChange}
+            min="O"
+            type="number"
+            id="salary2"
+            name="salary2"
+            value={formData.salary2}
+          />
+        </div>
+        <div className="formWrap">
+        <label>Your deposit:</label>
+        <input
+            onChange={handleChange}
+            min="O"
+            type="number"
+            id="deposit"
+            name="deposit"
+            value={formData.deposit}
+          />
+        </div>
+        <input onClick={handleSubmit} type='submit' value='calculate'/>
+      </form>
+    </div>
+  );
 };
 export default MortgageForm;
