d95aa8a9050194d58836e0b6193dc1bc13ef5d46
mirkiy <mirizachar@gmail.com>
2023-06-05 16:26:20 +0100

    calculation fix

--- src/components/MortgageDisplay.js
+++ src/components/MortgageDisplay.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 const MortgageDisplay = ({ maxAmount }) => {
-  if (!maxAmount) {
-    return null;
-  }
-  const formattedAmount = maxAmount.toLocaleString(); 
-
-  return <div className="mortgageDisplay">
-       <div className="formWrap">
-        <label>The amount you can borrow:</label>
-        <input
-            type='button'
-            id="maxAmount"
-            name="maxAmount"
-            defaultValue={formattedAmount}
-          />
+    if (isNaN(maxAmount) || maxAmount === undefined) {
+      return null;
+    }
+  
+    const formattedAmount = parseInt(maxAmount).toLocaleString();
+  
+    return (
+      <div className="mortgageDisplay">
+        <div className="formWrap">
+          <label>The amount you can borrow:</label>
+          <input type="button" id="maxAmount" name="maxAmount" value={formattedAmount} readOnly />
         </div>
-  </div>;
-};
+      </div>
+    );
+  };
+  
+
 export default MortgageDisplay;
