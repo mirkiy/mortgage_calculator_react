c176dd3f9aaacddd8300704947178c0eedf90ef8
mirkiy <mirizachar@gmail.com>
2023-06-06 15:57:45 +0100

    work on displaying the amount

--- src/components/MortgageDisplay.js
+++ src/components/MortgageDisplay.js
@@ -1,21 +1,26 @@
 import React from "react";
 
 const MortgageDisplay = ({ maxAmount }) => {
-    if (isNaN(maxAmount) || maxAmount === undefined) {
-      return null;
-    }
-  
-    const formattedAmount = parseInt(maxAmount).toLocaleString();
-  
-    return (
-      <div className="mortgageDisplay">
-        <div className="formWrap">
-          <label>The amount you can borrow:</label>
-          <input type="button" id="maxAmount" name="maxAmount" value={formattedAmount} readOnly />
-        </div>
+  if (maxAmount === undefined) {
+    return null;
+  }
+
+  const formattedAmount = maxAmount.toLocaleString(); // Format the value with commas as thousands separators
+
+  return (
+    <div className="mortgageDisplay">
+      <div className="formWrap">
+        <label>The amount you can borrow:</label>
+        <input
+          type="button"
+          id="maxAmount"
+          name="maxAmount"
+          value={formattedAmount}
+          readOnly
+        />
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
 
 export default MortgageDisplay;
